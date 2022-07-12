50502ed1278f781940be26eaf70d09259f7125a8
Xiaobei <yibaiyilan@gmail.com>
2022-07-12 23:14:48 +0000

    I didnt update the barchart

--- app.py
+++ app.py
@@ -3,48 +3,50 @@
 import dash
 from dash import html
 from dash import dcc
-import plotly.graph_objs as go
+import plotly.graph_objects as go
+import numpy as np
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
+color1='light blue'
+color2='pink'
+mytitle='This a Random 3d Chart!'
 
-label1='IBU'
-label2='ABV'
+xtitle='The Weight of August'
+ytitle='How Happy August is'
+ztitle='How Much August Ate'
+N = 50
 
 ########### Set up the chart
 
-def make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytitle):
-    bitterness = go.Bar(
-        x=beers,
-        y=ibu_values,
-        name=label1,
-        marker={'color':color1}
-    )
-    alcohol = go.Bar(
-        x=beers,
-        y=abv_values,
-        name=label2,
-        marker={'color':color2}
-    )
+def make_that_cool_chart(N, color1, color2, mytitle,xtitle,ytitle,ztitle):
+    fig = go.Figure()
+    fig.add_trace(go.Mesh3d(x=(60*np.random.randn(N)),
+                       y=(25*np.random.randn(N)),
+                       z=(40*np.random.randn(N)),
+                       opacity=0.5,
+                       color=color1
+                      ))
+    fig.add_trace(go.Mesh3d(x=(70*np.random.randn(N)),
+                       y=(55*np.random.randn(N)),
+                       z=(30*np.random.randn(N)),
+                       opacity=0.5,
+                       color=color2
+                      ))
 
-    beer_data = [bitterness, alcohol]
-    beer_layout = go.Layout(
-        barmode='group',
-        title = mytitle
-    )
-
-    beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-    return beer_fig
+    fig.update_layout(scene = dict(
+                        xaxis_title=xtitle,
+                        yaxis_title=ytitle,
+                        zaxis_title=ztitle),
+                        width=700,
+                        margin=dict(r=20, b=10, l=10, t=10),
+                        title=mytitle)
+    return fig
 
 
 ######### Run the function #######
 
 if __name__ == '__main__':
-    fig = make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytitle)
+    fig = make_that_cool_chart(N, color1, color2, mytitle,xtitle,ytitle,ztitle)
     fig.write_html('docs/barchart.html')
     print('We successfully updated the barchart!')
+
